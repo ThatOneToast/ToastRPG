641b9fe8d6cbc792582608b3b5ddae0eed9e15a9
ToastARG <101076597+ToastArgumentative@users.noreply.github.com>
2023-11-12 21:13:09 -0500

    Small Fix

--- src/main/java/toast/pine/Classes/Skills/Skill.java
+++ src/main/java/toast/pine/Classes/Skills/Skill.java
@@ -9,15 +9,19 @@ public abstract class Skill extends CharacterClass implements SkillInterface {
 
     protected final String skillName;
     protected final CharacterClass intendedClassUse;
+    protected final SkillAOE skillAOE;
+    protected final SkillDuration skillDuration;
     protected final List<String> skillDescription;
     protected final int skillLevel;
     protected final int skillMaxLevel;
     protected final int skillCost;
     protected final int skillCooldown;
 
-    public Skill(String skillName, CharacterClass intendedClassUse, List<String> skillDescription, int skillLevel, int skillMaxLevel, int skillCost, int skillCooldown) {
+    public Skill(String skillName, SkillAOE skillAOE, SkillDuration skillDuration, CharacterClass intendedClassUse, List<String> skillDescription, int skillLevel, int skillMaxLevel, int skillCost, int skillCooldown) {
         super(intendedClassUse.getName(), intendedClassUse.getIcon(), intendedClassUse.getHealth(), intendedClassUse.getDamage(), intendedClassUse.getMaxMana(), intendedClassUse.getManaPerSec(), intendedClassUse.getDefense(), intendedClassUse.getMaxLevel());
         this.skillName = skillName;
+        this.skillAOE = skillAOE;
+        this.skillDuration = skillDuration;
         this.intendedClassUse = intendedClassUse;
         this.skillDescription = skillDescription;
         this.skillLevel = skillLevel;
@@ -28,6 +32,14 @@ public abstract class Skill extends CharacterClass implements SkillInterface {
 
     public String getSkillName() {
         return skillName;
+    }
+
+    public SkillAOE getSkillAOE() {
+        return skillAOE;
+    }
+
+    public SkillDuration getSkillDuration() {
+        return skillDuration;
     }
 
     public CharacterClass getIntendedClassUse() {

8860f74d3730a47d82f1e7a140f0f9e4b533b6a4
ToastArgumentative <toastandbuttetbread@gmail.com>
2023-11-27 17:05:49 -0500

    missed

--- src/main/java/pine/toast/toastrpg/ToastRPG.kt
+++ src/main/java/pine/toast/toastrpg/ToastRPG.kt
@@ -69,12 +69,9 @@ object ToastRPG {
      * Run this method in your onDisable()
      */
     fun takePluginFromToast() {
-        if (!worldEventManager!!.getWorldEventsFile().exists()) return
-        if (!worldEventManager!!.unRegisterAllWorldEvents()) {
-            println("Successfully saved all world events.")
-        }
 
         manaRegen!!.stopManaUpdateTask()
+        
     }
 
     fun getEntityManager(): EntityManager? {

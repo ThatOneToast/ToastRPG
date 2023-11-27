fdcd904e02454c0618baa38b8c4772c17e82fc9c
ToastArgumentative <toastandbuttetbread@gmail.com>
2023-11-27 17:37:26 -0500

    Better method for getting what stage the event is at Trying to circumvent starting and canceling events.

--- src/main/java/pine/toast/toastrpg/worldevents/WorldEventManager.kt
+++ src/main/java/pine/toast/toastrpg/worldevents/WorldEventManager.kt
@@ -6,94 +6,58 @@ import pine.toast.toastrpg.events.WorldEventAlertNowEvent
 
 class WorldEventManager {
 
-   private var scheduledWorldEvents: HashMap<WorldEvent, WorldEventTime> = HashMap()
+    private var scheduledWorldEvents: HashMap<WorldEvent, WorldEventTime> = HashMap()
 
-   private fun registerWorldEvent(worldEvent: WorldEvent) {
-      val worldEventTime: WorldEventTime = worldEvent.getSpawnTime()
-      scheduledWorldEvents[worldEvent] = worldEventTime
-   }
+    private fun registerWorldEvent(worldEvent: WorldEvent) {
+        val worldEventTime: WorldEventTime = worldEvent.getSpawnTime()
+        scheduledWorldEvents[worldEvent] = worldEventTime
+    }
 
-   private fun unRegisterWorldEvent(worldEvent: WorldEvent) {
-      scheduledWorldEvents.remove(worldEvent)
-   }
+    private fun unRegisterWorldEvent(worldEvent: WorldEvent) {
+        scheduledWorldEvents.remove(worldEvent)
+    }
 
-   /**
-    * Starts scheduling the world event.
-    * @param worldEvent the world event to schedule.
-    *
-    */
-   fun startWorldEvent(worldEvent: WorldEvent) {
-      val eventStartTime: Long = worldEvent.getSpawnTime().getStartDate()
-      val currentTime: Long = System.currentTimeMillis()
-      registerWorldEvent(worldEvent)
-      ToastRPG.getPassedPlugin()!!.logger.info("Started scheduling world event ${worldEvent.getName()}.")
+    /**
+     * Starts scheduling the world event.
+     * @param worldEvent the world event to schedule.
+     *
+     */
+    fun startWorldEvent(worldEvent: WorldEvent) {
+        val eventStartTime: Long = worldEvent.getSpawnTime().getStartDate()
+        val currentTime: Long = System.currentTimeMillis()
+        registerWorldEvent(worldEvent)
+        ToastRPG.getPassedPlugin()!!.logger.info("Started scheduling world event ${worldEvent.getName()}.")
 
-      when {
-         eventStartTime - currentTime > 86400000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.ONE_DAY
-         )
-         )
-         eventStartTime - currentTime > 43200000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.TWELVE_HOURS
-         )
-         )
-         eventStartTime - currentTime > 21600000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.SIX_HOURS
-         )
-         )
-         eventStartTime - currentTime > 3600000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.ONE_HOUR
-         )
-         )
-         eventStartTime - currentTime > 1800000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.THIRTY_MINUTES
-         )
-         )
-         eventStartTime - currentTime > 900000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.FIFTEEN_MINUTES
-         )
-         )
-         eventStartTime - currentTime > 300000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.FIVE_MINUTES
-         )
-         )
-         eventStartTime - currentTime > 60000 -> ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(
-            WorldEventAlertEvent(worldEvent,
-            WorldEventStage.ONE_MINUTE
-         )
-         )
-         eventStartTime >= currentTime -> {
-            ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(WorldEventAlertNowEvent(worldEvent))
-            return
+        val delayMillis = eventStartTime - currentTime
 
-         }
-         worldEvent.getSpawnTime().isExpired() -> {
-            ToastRPG.getPassedPlugin()!!.logger.info("World event ${worldEvent.getName()} has expired.")
-            unRegisterWorldEvent(worldEvent)
-         }
+        if (delayMillis > 0) {
+            ToastRPG.getPassedPlugin()!!.server.scheduler.scheduleSyncDelayedTask(
+                ToastRPG.getPassedPlugin()!!,
+                {
+                    if (!worldEvent.getSpawnTime().isExpired()) {
+                        ToastRPG.getPassedPlugin()!!.logger.info("World event ${worldEvent.getName()} is starting now!")
+                        ToastRPG.getPassedPlugin()!!.server.pluginManager.callEvent(WorldEventAlertNowEvent(worldEvent))
+                    } else {
+                        ToastRPG.getPassedPlugin()!!.logger.info("World event ${worldEvent.getName()} has expired during the delay.")
+                        unRegisterWorldEvent(worldEvent)
+                    }
+                },
+                delayMillis / 50
+            )
+        } else {
+            ToastRPG.getPassedPlugin()!!.logger.warning("World event ${worldEvent.getName()} start time has already passed.")
+        }
+    }
 
-      }
 
-   }
+    /**
+     * Stops scheduling the world event.
+     * @param worldEvent the world event to stop scheduling.
+     */
+    fun stopWorldEvent(worldEvent: WorldEvent) {
+        worldEvent.getSpawnTime().setExpired()
+    }
 
-   /**
-    * Stops scheduling the world event.
-    * @param worldEvent the world event to stop scheduling.
-    */
-   fun stopWorldEvent(worldEvent: WorldEvent) {
-      worldEvent.getSpawnTime().setExpired()
-   }
-
-   fun getScheduledWorldEvents(): HashMap<WorldEvent, WorldEventTime> {
-      return scheduledWorldEvents
-   }
 
 
 

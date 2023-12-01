e106fbb1e31fd82f032576c636bff1a518c4b3f8
ToastArgumentative <toastandbuttetbread@gmail.com>
2023-12-01 12:06:35 -0500

    Stupid repeating

--- src/main/java/pine/toast/toastrpg/library/Keys.kt
+++ src/main/java/pine/toast/toastrpg/library/Keys.kt
@@ -3,6 +3,6 @@ package pine.toast.toastrpg.library
 import org.bukkit.NamespacedKey
 
 object Keys {
-    val entityHandler: NamespacedKey = NamespacedKey("ToastRPG", "entityhandler")
-    val livingEntityHandler: NamespacedKey = NamespacedKey("ToastRPG", "livingentityhandler")
+    val entityHandler: NamespacedKey = NamespacedKey("toastrpg", "entityhandler")
+    val livingEntityHandler: NamespacedKey = NamespacedKey("toastrpg", "livingentityhandler")
 }
